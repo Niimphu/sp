2081f074c92c97998940cbc6be9073f9b7ed23a4
Niimphu <nimphu@outlook.com>
2026-03-20 14:08:47 +0000

    menus

---
+++ main_menu.gd
@@ -0,0 +1,29 @@
+extends Control
+
+@export var main: Control
+@export var play_menu: Control
+
+@export var play_button: Button
+@export var options_button: Button
+@export var quit_button: Button
+
+
+func _ready() -> void:
+	play_menu.visible = false
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_play_button_pressed() -> void:
+	main.visible = false
+	play_menu.visible = true
+
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
